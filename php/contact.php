9133dc694820cf2574220807d475db8f79297f5b
Camden Wagner <awesomemancam@gmail.com>
2016-01-13 13:28:12 -0500

    Contact Us & reg

--- php/contact.php
+++ php/contact.php
@@ -5,29 +5,29 @@ File: contact.php
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<!-- Website Title & Description -->
-		<title>Contact</title>
-		<meta charset="utf-8">
-		<meta name="description" content="">
-		
-		<link rel="shortcut icon" href="img/e300.ico" type="e300/ico" />
+<head>
+	<!-- Website Title & Description -->
+	<title>Contact</title>
+	<meta charset="utf-8">
+	<meta name="description" content="">
 
-		<!-- Mobile viewport optimized -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="shortcut icon" href="img/e300.ico" type="e300/ico" />
 
-		<!-- Bootstrap CSS -->
-		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link href="../includes/css/bootstrap-glyphicons.css" rel="stylesheet">
+	<!-- Mobile viewport optimized -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-		<!-- Custom CSS -->
-		<link href="../includes/css/styles.css" rel="stylesheet">
+	<!-- Bootstrap CSS -->
+	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../includes/css/bootstrap-glyphicons.css" rel="stylesheet">
 
-		<!-- Include Modernizr in the head, before any other Javascript -->
-		<script src="../includes/js/modernizr-2.6.2.min.js"></script>
-	</head>
-	<body>
-		<div class="background">
+	<!-- Custom CSS -->
+	<link href="../includes/css/styles.css" rel="stylesheet">
+
+	<!-- Include Modernizr in the head, before any other Javascript -->
+	<script src="../includes/js/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+	<div class="background">
 		<!-- Layout -->
 		<div class="container" id="main">
 
@@ -45,31 +45,33 @@ File: contact.php
 
 			<hr>
 
-			<img src="./img/e300_banner.jpg" class="pull-right">
+			<center><img src="./img/e300_banner.jpg" class="pull-right" style="margin-bottom:30px; margin-right:74px;">
 
 			<h2>Contact <a href="./staff/ewagner.php" style="color: #FFFFFF;"><i>Eric Wagner</i></a></h2>
 
 			<h4><b>Email:  </b>ewagner1@swarthmore.edu</h4>
-			<h4 style="margin-bottom: 500px;"><b>Phone:  </b>610-690-6882 (work)</h4>
+			<h4><b>Phone:  </b>610-690-6882 (work)</h4></center>
+
+			<center><img src="./img/frombleachers.jpg"></center>
 
 			<hr>
 
 
 		</div><!-- end container -->
 
-		</div><!--end background -->
+	</div><!--end background -->
 
-		<?php include './footer.php';?>
+	<?php include './footer.php';?>
 
-		<!-- All Javascript at the bottom of the page for faster page loading -->
-		<!-- First try for the online version of jQuery-->
-		<script src="http://code.jquery.com/jquery.js"></script>
-		<!-- If no online access, fallback to our hardcoded version of jQuery -->
-		<script>window.jQuery || document.write('<script src="../includes/js/jquery-1.8.2.min.js"><\/script>')</script>
-		<!-- Bootstrap JS -->
-		<script src="../bootstrap/js/bootstrap.min.js"></script>
-		<!-- Custom JS -->
-		<script src="../includes/js/script.js"></script>
+	<!-- All Javascript at the bottom of the page for faster page loading -->
+	<!-- First try for the online version of jQuery-->
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<!-- If no online access, fallback to our hardcoded version of jQuery -->
+	<script>window.jQuery || document.write('<script src="../includes/js/jquery-1.8.2.min.js"><\/script>')</script>
+	<!-- Bootstrap JS -->
+	<script src="../bootstrap/js/bootstrap.min.js"></script>
+	<!-- Custom JS -->
+	<script src="../includes/js/script.js"></script>
 
-	</body>
+</body>
 </html>
